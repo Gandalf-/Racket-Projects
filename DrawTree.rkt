02de790547e60a32c6c293d08b2de57afee6d209
Gandalf- <austin.voecks@gmail.com>
2015-05-01 12:50:34 -0700

    Better comments and spacing for DrawTree and Wander

---
+++ DrawTree.rkt
@@ -0,0 +1,190 @@
+#lang racket
+
+(require racket/gui)
+
+;GUI INIT
+;---------------------------------------------
+(define frame-width 600)
+(define frame-height 400)
+
+(define depth 25)
+
+;Frame
+(define frame
+  (new frame%
+       (label "Draw Tree")
+       (min-width frame-width)
+       (min-height (+ 20 frame-height))))
+
+(send frame create-status-line)
+(send frame show #t)
+
+;Panel for choices bar
+(define top-panel
+  (new horizontal-panel%
+       (parent frame)
+       (stretchable-height #f)))
+
+(define bottom-panel
+  (new horizontal-panel%
+       (parent frame)
+       (stretchable-height #f)
+       (style '(border))
+       (border 0)))
+
+;Sliders
+;-----------------------------------------------
+
+;Height Change
+(define y-change-min 10)
+(define y-change-max 20)
+
+(define height-slider
+  (new slider%
+       (label "Height")
+       (parent top-panel)
+       (min-value y-change-min)
+       (max-value y-change-max)
+       (init-value 15)))
+
+;Branch spread
+(define branch-min 5)
+(define branch-max 25)
+
+(define spread-slider
+  (new slider%
+       (label "Spread")
+       (parent top-panel)
+       (min-value branch-min)
+       (max-value branch-max)
+       (init-value 10)))
+
+;Fancy color
+(define color-button
+  (new check-box%
+       (parent top-panel)
+       (label "Color")))
+
+;Branch chance
+(define branch-slider
+  (new slider%
+       (label "Branch")
+       (parent bottom-panel)
+       (min-value 0)
+       (max-value 100)
+       (init-value 60)))
+
+;Decay chance
+(define decay-slider
+  (new slider%
+       (label "Decay")
+       (parent bottom-panel)
+       (min-value 0)
+       (max-value 10)
+       (init-value 5)))
+
+;Refresh button
+(define refresh-button
+  (new button%
+       (parent bottom-panel)
+       (label "Regenerate")
+       (callback (lambda (b e) (send canvas refresh)))))
+
+;Draw tree
+;-----------------------------------------
+(define continue-decay 5)
+
+(define root
+  (make-object point% (/ frame-width 2) frame-height))
+
+(define color-brown (make-object color% 125 65 0))
+(define color-green (make-object color% 0 50 0))
+(define color-black (make-object color% 0 0 0))
+
+(define brown-pen (make-object pen% color-brown))
+(define green-pen (make-object pen% color-green))
+(define black-pen (make-object pen% color-black))
+
+(define my-draw
+  (lambda (canvas dc)
+
+    (define (make-tree parent depth max-depth continue-chance)
+
+      ;Colors?
+      (if (send color-button get-value)
+        (begin
+          (send dc set-pen brown-pen)
+
+          ;Make the top 3/4 green
+          (when (>= depth (/ max-depth 4))
+            (send dc set-pen green-pen)))
+
+        (send dc set-pen black-pen))
+
+      ;Done?
+      (unless (= depth max-depth)
+        ;Continue?
+        (when (>= (/ continue-chance 100) (random))
+
+          ;Branches or stem?
+          (if (>= (/ (send branch-slider get-value) 100) (random))
+
+            ;Branches
+            (let ((left-child (make-object
+                                point% 
+                                ;Random x change from slider
+                                (- (send parent get-x)
+                                   (+ branch-min
+                                      (random (send spread-slider get-value))))
+
+                                ;Random y change from slider
+                                (- (send parent get-y)
+                                   (+ y-change-min
+                                      (random (send height-slider get-value))))))
+
+                  (right-child (make-object 
+                                 point%
+                                 ;Random x change from slider
+                                 (+ (send parent get-x)
+                                    (+ branch-min 
+                                       (random (send spread-slider get-value))))
+
+                                 ;Random y change from slider
+                                 (- (send parent get-y)
+                                    (+ y-change-min 
+                                       (random (send height-slider get-value)))))))
+
+              ;Draw lines between children and parent
+              (send dc draw-lines (list parent left-child))
+              (send dc draw-lines (list parent right-child))
+
+              ; Recurse
+              (make-tree left-child (+ depth 1) max-depth (- continue-chance (send decay-slider get-value)))
+              (make-tree right-child (+ depth 1) max-depth (- continue-chance (send decay-slider get-value))))
+
+            ;Stem
+            (let ((stem (make-object
+                          point%
+                          ;No x change
+                          (send parent get-x)
+                          ;Random y change from slider
+                          (- (send parent get-y)
+                             (+ y-change-min 
+                                (random (send height-slider get-value)))))))
+
+              ; Draw line between old stem segment and new one
+              (send dc draw-lines (list parent stem))
+
+              ; Recurse
+              (make-tree stem (+ depth 1) max-depth (- continue-chance (send decay-slider get-value))))
+            ))))
+
+    (make-tree root 0 depth 100)))
+
+;Canvas
+(define canvas
+  (new canvas%
+       (parent frame)
+       (min-width frame-width)
+       (min-height frame-height)
+       (paint-callback my-draw)))
--- Wander.rkt
+++ Wander.rkt
@@ -6,15 +6,15 @@
 ;---------------------------------------------
 (define frame-width 600)
 (define frame-height 400)
-
 (define move-distance 20)
 (define dot-size 20)
 
 ;Frame
-(define frame (new frame%
-                   (label "Wandering Worm")
-                   (min-width frame-width)
-                   (min-height frame-height)))
+(define frame
+  (new frame%
+       (label "Wandering Worm")
+       (min-width frame-width)
+       (min-height frame-height)))
 
 (send frame create-status-line)
 (send frame show #t)
@@ -25,12 +25,12 @@
        (parent frame)
        (stretchable-height #f)
        (style '(border))
-       (border 2)
-       ))
+       (border 2) ))
 
-;Choose number of segments in body
+;Choose number of segments in worm body
 (define num-segments 20)
 
+;Allow changes to the number of segments in real time
 (define segment-slider
   (new slider%
        (parent h-panel)
@@ -38,14 +38,16 @@
        (min-value 1)
        (max-value 40)
        (init-value 10)
-       (callback (lambda (b e) 
-                   (set! num-segments (send segment-slider get-value))
-                   ;Check for crash values
-                   (when (and (> num-segments 20) (= (send speed-slider get-value) 1))
-                     (send speed-slider set-value (+ (send speed-slider get-value) 1)))
-                   ;Remake the body
-                   (fill-body)
-                   (send canvas refresh)))))
+       ;Callback means that this function is called whenever segment-slider's value changes
+       (callback 
+         (lambda (button event) 
+           (set! num-segments (send segment-slider get-value))
+           ;Check for crash values
+           (when (and (> num-segments 20) (= (send speed-slider get-value) 1))
+             (send speed-slider set-value (+ (send speed-slider get-value) 1)))
+           ;Remake the body
+           (fill-body)
+           (send canvas refresh)))))
 
 ;Choose animation speed
 (define speed-slider
@@ -55,11 +57,12 @@
        (min-value 1)
        (max-value 10)
        (init-value 4)
-       (callback (lambda (b e)
-                   ;Check for crash values
-                   (when (and (> num-segments 20) (= (send speed-slider get-value) 1))
-                     (send speed-slider set-value (+ (send speed-slider get-value) 1)))))
-       ))
+       (callback 
+         (lambda (b e)
+           ;Check for crash values
+           (when (and (> num-segments 20)
+                      (= (send speed-slider get-value) 1))
+             (send speed-slider set-value (+ (send speed-slider get-value) 1)))))))
 
 ;Choose head's new position angle
 (define spread-slider
@@ -81,29 +84,33 @@
 ; a later change the number of segments
 (define (fill-body)
   (set! body '())
-  
+
   ;The first call can't access canvas, so use default frame values
   (if (equal? #f init-done)
-      (begin
-        (let loop ((i 0))
-          (unless (= i num-segments)
-            (let ((head (make-object point% (random frame-width) (random frame-height))))
-              (set! body (append body (list head))))
-            
-            (loop (+ i 1))))
-        (set! init-done #t))
-      ;This is called when the init has been finished and the user changes
-      ; the size of the window. The random starting postions of the dots
-      ; now take up the 
+    (begin
       (let loop ((i 0))
         (unless (= i num-segments)
-          (let ((head (make-object point% 
-                        (random (send canvas get-width))
-                        (random (send canvas get-height)))))
+          (let ((head (make-object
+                        point%
+                        (random frame-width)
+                        (random frame-height))))
             (set! body (append body (list head))))
-          
+
           (loop (+ i 1))))
-      ))
+      (set! init-done #t))
+
+    ;This is called when the init has been finished and the user changes
+    ; the size of the window. The random starting postions of the dots
+    ; now take up the 
+    (let loop ((i 0))
+      (unless (= i num-segments)
+        (let ((head (make-object
+                      point% 
+                      (random (send canvas get-width))
+                      (random (send canvas get-height)))))
+          (set! body (append body (list head))))
+
+        (loop (+ i 1)))) ))
 
 (fill-body)
 
@@ -123,68 +130,69 @@
 
 (define instructions%
   (class object%
-    (public move my-draw)
-    
-    ;Draw body- draw an ellipse at the point given by each element
-    ; of the body list
-    (define (my-draw dc)
-      (send dc set-brush brush-orange)
-      
-      (for-each (lambda (point)
-                  (send dc draw-ellipse 
-                        (send point get-x)
-                        (send point get-y)
-                        dot-size
-                        dot-size))
-                body))
-    
-    ;Move body
-    ; Move all but the first element to the position of the element before it
-    ; Choose a new position for the first element, and move it there
-    (define (move)
-      
-      (define (find-new-x angle)
-        (* (cos angle) move-distance))
-      
-      (define (find-new-y angle)
-        (* (sin angle) move-distance))
-      
-      (define (random-sign)
-        (if (>= .5 (random))
-            1
-            -1))
-      
-      ;Slave function that recursively goes through the body list
-      (define (move-slave input)
-        (unless (empty? input)
-          (if (equal? (car input) (car body))
-              ;Head case, choose new postion and move head there
-              (begin                 
-                (let ((angle (+ previous-angle (* (random-sign) (+ (/ (send spread-slider get-value) 10) (random 1))))))
-                ;(let ((angle (- previous-angle (* (random-sign) (random (send spread-slider get-value))))))
-                  
-                  ;Assign nex x coord
-                  (send (car input) set-x (modulo (round (+ (send (car input) get-x)
-                                                            (find-new-x angle)))
-                                                  (send canvas get-width)))
-                  ;Assign new y coord
-                  (send (car input) set-y (modulo (round (+ (send (car input) get-y)
-                                                            (find-new-y angle)))
-                                                  (send canvas get-height)))
-                  (set! previous-angle angle)))
-              ;Body case, set everything to next in reversed list
-              (begin
-                (send (car input) set-x (send (car (cdr input)) get-x))
-                (send (car input) set-y (send (car (cdr input)) get-y))
-                (move-slave (cdr input))))))
-      
-      (move-slave (reverse body))
-      )
-    (super-new)))
+         (public move my-draw)
+
+         ;Draw body- draw an ellipse at the point given by each element
+         ; of the body list
+         (define (my-draw dc)
+           (send dc set-brush brush-orange)
+
+           (for-each 
+             (lambda (point)
+               (send dc draw-ellipse 
+                     (send point get-x)
+                     (send point get-y)
+                     dot-size
+                     dot-size))
+             body))
+
+         ;Move body
+         ; Move all but the first element to the position of the element before it
+         ; Choose a new position for the first element, and move it there
+         (define (move)
+           (define (find-new-x angle)
+             (* (cos angle) move-distance))
+
+           (define (find-new-y angle)
+             (* (sin angle) move-distance))
+
+           (define (random-sign)
+             (if (>= .5 (random))
+               1
+               -1))
+
+           ;Slave function that recursively goes through the body list
+           (define (move-slave input)
+             (unless (empty? input)
+               (if (equal? (car input) (car body))
+                 ;Head case, choose new postion and move head there
+                 (begin                 
+                   (let ((angle (+ previous-angle 
+                                   (* (random-sign)
+                                      (+ (/ (send spread-slider get-value)
+                                            10)
+                                         (random 1))))))
+                     ;Assign nex x coord
+                     (send (car input) set-x (modulo (round (+ (send (car input) get-x)
+                                                               (find-new-x angle)))
+                                                     (send canvas get-width)))
+                     ;Assign new y coord
+                     (send (car input) set-y (modulo (round (+ (send (car input) get-y)
+                                                               (find-new-y angle)))
+                                                     (send canvas get-height)))
+                     (set! previous-angle angle)))
+                 ;Body case, set everything to next in reversed list
+                 (begin
+                   (send (car input) set-x (send (car (cdr input)) get-x))
+                   (send (car input) set-y (send (car (cdr input)) get-y))
+                   (move-slave (cdr input))))))
+
+           (move-slave (reverse body)))
+
+         (super-new)))
 
 ;Create instructions object
 (define instructions (new instructions%))
-
 
 ;RUN BOX, CANVASES
 ;---------------------------------------------
@@ -193,28 +201,27 @@
   (new check-box%
        (label "Run")
        (parent h-panel)
+       ; Callback is called whenever the state of the check-box changes
        (callback
-        (lambda (checkbox event)
-          (if (send checkbox get-value)
-              (thread-resume animate)
-              (thread-suspend animate)))
-        )))
+         (lambda (checkbox event)
+           (if (send checkbox get-value)
+             (thread-resume animate)
+             (thread-suspend animate))))))
 
 ;Canvases
 (define my-canvas%
   (class canvas%
-    (override on-paint)
-    
-    ;Clear canvas then redraw using (my-draw)
-    (define (on-paint)
-      (let ((dc (send this get-dc)))
-        (send dc clear)
-        (send instructions my-draw dc)))
-    
-    (super-new)
-    (send (send this get-dc) set-background
-          color-green)
-    ))
+         (override on-paint)
+
+         ;Clear canvas then redraw using (my-draw)
+         (define (on-paint)
+           (let ((dc (send this get-dc)))
+             (send dc clear)
+             (send instructions my-draw dc)))
+
+         (super-new)
+         (send (send this get-dc) set-background
+               color-green)))
 
 (define canvas
   (new my-canvas%
@@ -227,11 +234,12 @@
 
 (define animate
   (thread
-   (lambda ()
-     (let loop ((i 0))
-       (send instructions move)
-       (send canvas refresh)
-       (sleep (/ (send speed-slider get-value) 50))
-       (loop (+ 1 i))))))
+    (lambda ()
+      (let loop ((i 0))
+        (send instructions move)
+        (send canvas refresh)
+        (sleep (/ (send speed-slider get-value)
+                  50))
+        (loop (+ 1 i))))))
 
 (thread-suspend animate)
