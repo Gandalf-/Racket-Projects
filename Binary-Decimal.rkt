8d3f34004b1afd2cb47b487d8172381dea919fcc
Gandalf- <austin.voecks@gmail.com>
2016-04-13 13:43:19 -0700

    Much better comments and demo for Binary-Decimal

--- Binary-Decimal.rkt
+++ Binary-Decimal.rkt
@@ -4,11 +4,13 @@
 (provide decimal->binary)
 (provide padding)
 
-(define padding 8)  ; number of bits in a character
+; number of bits in a character, makes numbers uniform width.
+(define padding 8)  
 
 ; Converts a binary number represented as a string to it's decimal equivalent
 ;
 ; string -> number
+; (binary->decimal "00000010") -> 2
 (define (binary->decimal input)
   (let convert ((list-chars (reverse
                               (string->list input)))
@@ -29,76 +31,86 @@
       )))
 
 
+;Find the largest two's power in the given input
+;
+; number -> number
+; (find-highest-place 567) -> 512
+(define (find-highest-place input)
+  (let loop ((current 1)
+             (previous 0))
+    (if (< input current)
+      previous
+      (loop (* 2 current) current))))
+
+;Find all the factors of powers of two in the input
+;
+; number -> list of number
+; (find-places 567) -> (1 2 4 16 32 512)
+(define (find-places input)
+  (let loop ((output '())
+             (current-place input))
+    (if (= current-place 0)
+      (reverse output)
+      (loop (flatten (cons output (find-highest-place current-place)))
+            (- current-place (find-highest-place current-place)))
+      )))
+
+; Add padding a binary number. Padding amount declared globally
+;
+; list of number -> list of number
+; (add-pad '(1 0)) -> '(0 0 0 0 0 0 1 0)
+(define (add-pad in)
+  (if (> (length in) padding)
+    in
+    (append
+      (build-list (- padding (length in))
+                  (lambda (x) 0))
+      in)))
+
+; Turn a list of 1's and 0's into a string of '1's and '0's
+;
+; list of number -> string
+; (numlist->string '(0 0 0 0 0 0 1 0)) -> "00000010"
+(define (numlist->string in)
+  (list->string
+    (map
+      (lambda (x) 
+        (if (= x 1) 
+          #\1 
+          #\0))
+      (add-pad (reverse in)))))
+
+; Check each power of two for a corresponding value in the places list,
+; does the actual work here. Utilizes the above helper functions
+;
+; number -> string
+; (make-output '(1 2 16 32)) -> "00110011"
+(define (make-output input)
+  (let loop ((current-place 1)
+             (input-list input)
+             (output '() ))
+
+    (if (empty? input-list)
+      (numlist->string output)
+
+      (if (= (car input-list) current-place)
+        ; input has this power of 2, add a one
+        (loop (* 2 current-place)
+              (cdr input-list)
+              (flatten (cons output 1)))
+
+        ; input doesn't have this power of 2, add a zero
+        (loop (* 2 current-place)
+              input-list
+              (flatten (cons output 0)))
+        ))))
+
 ; Converts a decimal number to its binary equivalent represented in 
 ; a string of 1's and 0's
 ;
 ; number -> string
+; (decimal->binary 2) -> "00000010"
 (define (decimal->binary input)
-
-  ;Find the largest two's power in the given input
-  ;
-  ; number -> number
-  (define (find-highest-place input)
-    (let loop ((current 1)
-               (previous 0))
-      (if (< input current)
-        previous
-        (loop (* 2 current) current))))
-
-  ;Find all the factors of powers of two in the input
-  ;
-  ; number -> list of number
-  (define (find-places input)
-    (let loop ((output '())
-               (current-place input))
-      (if (= current-place 0)
-        (reverse output)
-        (loop (flatten (cons output (find-highest-place current-place)))
-              (- current-place (find-highest-place current-place)))
-        )))
-
-  ;Add padding
-  ;
-  ; list of number -> list of number
-  (define (add-pad in)
-    (append
-      (build-list (- padding (length in))
-                  (lambda (x) 0))
-      in))
-
-  ; Turn a list of 1's and 0's into a string of '1's and '0's
-  ;
-  ; list of number -> string
-  (define (numlist->string in)
-    (list->string
-      (map
-        (lambda (x) 
-          (if (= x 1) 
-            #\1 
-            #\0))
-        (add-pad (reverse in)))))
-
-  ; Check each power of two for a corresponding value in the places list,
-  ; does the actual work here. Utilizes the above helper functions
-  ;
-  ; number -> string
-  (define (make-output input)
-    (let loop ((current-place 1)
-               (input-list input)
-               (output '() ))
-
-      (if (empty? input-list)
-        (numlist->string output)
-
-        (if (= (car input-list) current-place)
-          (loop (* 2 current-place)
-                (cdr input-list)
-                (flatten (cons output 1)))
-
-          (loop (* 2 current-place)
-                input-list
-                (flatten (cons output 0)))
-          ))))
   (if (= 0 input)
     (make-string padding #\0)
     (make-output (find-places input))))
@@ -106,6 +118,12 @@
 
 (define (test)
   (displayln (binary->decimal "00000010"))
+  (displayln (find-highest-place 567))
+  (displayln (find-places 567))
+  (displayln (add-pad '(1 0)))
+  (displayln (numlist->string '(0 0 0 0 0 0 1 0)))
+  (displayln (make-output '(1 2 16 32)))
   (displayln (decimal->binary 2))
-  (displayln (decimal->binary 23498723498723))
-  (define input "Hello there. What's going on? Congratulations on decoding this!"))
+  (displayln (decimal->binary 23498723498723)))
+
+;(test)
