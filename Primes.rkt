21b36ea4160fd1b0456c4b5c0de21c944e0172d2
Gandalf- <austin.voecks@gmail.com>
2016-04-13 13:53:51 -0700

    Much better comments on Primes

--- Primes.rkt
+++ Primes.rkt
@@ -2,18 +2,37 @@
 
 (provide primes)
 
-; Returns all a list of all prime numbers less than 'limit'
+; Returns all a list of all prime numbers less than 'limit' 
+; using the Sieve of Eratosthenes. What we do is build a list of all
+; numbers up to the limit. Then we go through the list and do the following:
+;
+; while the input list is not empty:
+;   let x be the current number
+;   add x to the output list
+;   remove all other numbers in the input list that are divisible by x
+;   remove x from the input list
+;
+; number -> number
 (define (primes limit)
-  (let loop ((in (cddr
-                   (build-list limit values)))
-             (out '(1) ))
-    (if (empty? in)
-      (reverse out)
-      (loop (filter 
-              (lambda (x)
-                (not (= 0 (modulo x (car in)))))
-              (cdr in))
-            (cons (car in)
-                  out))) ))
+  (let loop 
+    ; build a list of all numbers up to the limit
+    ((input (cddr (build-list limit values)))
+     ; initialize our output list
+     (output '(1) ))
 
-(primes 10000000)
+    (if (empty? input)
+      ; building the list in reverse allows us to use 'cons', 
+      ; which is much faster than append
+      (reverse output)
+
+      (loop 
+        ; remove all other elements divisible by the current element
+        (filter 
+          (lambda (x) (not (= 0 (modulo x (car input)))))
+          (cdr input))
+
+        ; add the current element to the output
+        (cons (car input)
+              output))) ))
+
+(primes 10000)
