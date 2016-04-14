241ac35a107540660ccd6dbd01acdde02be4148c
Gandalf- <austin.voecks@gmail.com>
2016-04-13 19:22:59 -0700

    Divide-Precise handles negative numbers, does parameter checking

--- DividePrecise.rkt
+++ DividePrecise.rkt
@@ -2,7 +2,9 @@
 
 (provide divide-precise)
 
-; Divide (/ x y) with prec decimal places of accuracy
+; Divide (/ x y) with 'precision' decimal places of accuracy
+;
+; number, number, number -> string
 (define (divide-precise x y precision)
   
   (define (list-of-string->string x)
@@ -22,14 +24,28 @@
   
   (define (remain x y)
     (- x (* (string->number (quot x y)) y)))
-  
+
+  (define (str-abs x)
+    (number->string
+      (abs (string->number x))))
+
+  (unless (and (number? x) (number? y) (number? precision))
+    (error "divide-precise: invalid arguments"))
+
   (let loop ((out '() ) (i 0) (x x))
     (if (= i precision)
-        (list-of-string->string out)
-        (loop (if (= i 0)
-                  (cons "." (cons (quot x y) out))
-                  (cons (quot x y) out))
-              (+ i 1)
-              (* (remain x y) 10)))))
+      (list-of-string->string out)
 
-(divide-precise 10 12345 550)
+      (loop (if (= i 0)
+              (cons "." (cons (quot x y) out))
+              (cons (str-abs (quot x y)) out))
+            (+ i 1)
+            (* (remain x y) 10)))))
+
+(define (test)
+  (displayln (divide-precise 10 12345 550))
+  (displayln (divide-precise 22.2349 -7.123 1000))
+  (displayln (divide-precise -20 -4 100))
+  (displayln (divide-precise -20 '(1) 100)))
+
+(test)
